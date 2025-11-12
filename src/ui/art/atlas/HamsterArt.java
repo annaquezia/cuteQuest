804253411e24924e2ddf0cee51caaa96a28b7c11
Anna Quézia <142363490+annaquezia@users.noreply.github.com>
2025-11-12 18:12:30 -0300

    add: ui classes

---
+++ src/ui/art/atlas/HamsterArt.java
@@ -0,0 +1,250 @@
+package ui.art.atlas;
+
+public class HamsterArt {
+    private HamsterArt(){}
+
+    public static final String[] IDLE = {
+            "₍ᐢ⚈.⚈ᐢ₎ྀ◞ ̑̑"
+    };
+
+    public static final String[] FEED = {
+            "₍ᐢ⚈.⚈ᐢ₎っ🌰"
+    };
+
+    public static final String[] PLAY = {
+            "₍ᐢ⚈.⚈ᐢ₎っ🪀"
+    };
+
+    public static final String[] SLEEP = {
+            "₍ᐢ-.-ᐢ₎ z z"
+    };
+
+    public static final String[] SICK = {
+            "₍ᐢ×.-ᐢ₎💊"
+    };
+
+    public static final String[] HUNGRY = {
+            "₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "        ╭ ◜◝ ͡ ◜◝ ╮",
+            "        (   🌰   )",
+            "        ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED = {
+            "₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "        ╭ ◜◝ ͡ ◜◝ ╮",
+            "        (   zZZ  )",
+            "        ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HEAL = {
+            "₍ᐢ×.-ᐢ₎  +  💊  =  ₍ᐢ⚈.⚈ᐢ₎ྀ♡"
+    };
+
+    public static final String[] BOW = {
+            "🎀₍ᐢ⚈.⚈ᐢ₎"
+    };
+
+    public static final String[] SCARF = {
+            "₍ᐢ⚈.⚈ᐢ₎",
+            "  🧣  "
+
+    };
+
+    public static final String[] BATH = {
+            "🫧 ₍ᐢ⚈.⚈ᐢ₎ 🫧"
+    };
+
+    public static final String[] BATH_SCARF = {
+            "🫧₍ᐢ⚈.⚈ᐢ₎",
+            "    🧣  "
+    };
+
+    public static final String[] BATH_BOW = {
+            "🫧🎀₍ᐢ⚈.⚈ᐢ₎"
+    };
+
+    public static final String[] FEED_BOW = {
+            "🎀₍ᐢ⚈.⚈ᐢ₎っ🌰"
+    };
+
+    public static final String[] FEED_SCARF = {
+            "₍ᐢ⚈.⚈ᐢ₎っ🌰",
+            "  🧣"
+    };
+
+    public static final String[] FEED_BATH = {
+            "🫧₍ᐢ⚈.⚈ᐢ₎っ🌰"
+    };
+
+    public static final String[] FEED_BATH_SCARF = {
+            "₍ᐢ⚈.⚈ᐢ₎っ🌰",
+            "  🧣"
+    };
+
+    public static final String[] FEED_BATH_BOW = {
+            "🫧🎀₍ᐢ⚈.⚈ᐢ₎っ🌰"
+    };
+
+    public static final String[] PLAY_BOW = {
+            "🎀₍ᐢ⚈.⚈ᐢ₎っ🪀"
+    };
+
+    public static final String[] PLAY_SCARF = {
+            "₍ᐢ⚈.⚈ᐢ₎っ🪀",
+            "  🧣"
+    };
+
+    public static final String[] PLAY_BATH = {
+            "🫧₍ᐢ⚈.⚈ᐢ₎っ🪀"
+    };
+
+    public static final String[] PLAY_BATH_SCARF = {
+            "🫧₍ᐢ⚈.⚈ᐢ₎っ🪀",
+            "     🧣"
+    };
+
+    public static final String[] PLAY_BATH_BOW = {
+            "🫧🎀₍ᐢ⚈.⚈ᐢ₎っ🪀"
+    };
+
+    public static final String[] SLEEP_BOW = {
+            "🎀₍ᐢ-.-ᐢ₎ z z"
+    };
+
+    public static final String[] SLEEP_SCARF = {
+            "₍ᐢ-.-ᐢ₎ z z",
+            "  🧣"
+    };
+
+    public static final String[] SLEEP_BATH = {
+            "🫧₍ᐢ-.-ᐢ₎ z z"
+    };
+
+    public static final String[] SLEEP_BATH_SCARF = {
+            "🫧₍ᐢ-.-ᐢ₎ z z",
+            "    🧣"
+    };
+
+    public static final String[] SLEEP_BATH_BOW = {
+            "🫧🎀₍ᐢ-.-ᐢ₎ z z"
+    };
+
+    public static final String[] SICK_BOW = {
+            "🎀₍ᐢ×.-ᐢ₎💊"
+    };
+
+    public static final String[] SICK_SCARF = {
+            "₍ᐢ×.-ᐢ₎💊",
+            "  🧣"
+    };
+
+    public static final String[] SICK_BATH = {
+            "🫧₍ᐢ×.-ᐢ₎💊"
+    };
+
+    public static final String[] SICK_BATH_SCARF = {
+            "🫧₍ᐢ×.-ᐢ₎💊",
+            "    🧣"
+    };
+
+    public static final String[] SICK_BATH_BOW = {
+            "🫧🎀₍ᐢ×.-ᐢ₎💊"
+    };
+
+    public static final String[] HUNGRY_BOW = {
+            "🎀₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "          ╭ ◜◝ ͡ ◜◝ ╮",
+            "          (   🌰   )",
+            "          ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HUNGRY_SCARF = {
+            "₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "   🧣        ○",
+            "         ╭ ◜◝ ͡ ◜◝ ╮",
+            "         (   🌰   )",
+            "         ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HUNGRY_BATH = {
+            "🫧₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "          ╭ ◜◝ ͡ ◜◝ ╮",
+            "          (   🌰   )",
+            "          ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HUNGRY_BATH_SCARF = {
+            "🫧₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "     🧣        ○",
+            "           ╭ ◜◝ ͡ ◜◝ ╮",
+            "           (   🌰   )",
+            "           ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HUNGRY_BATH_BOW = {
+            "🫧🎀₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "            ╭ ◜◝ ͡ ◜◝ ╮",
+            "            (   🌰   )",
+            "            ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED_BOW = {
+            "🎀₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "        ╭ ◜◝ ͡ ◜◝ ╮",
+            "        (   zZZ  )",
+            "        ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED_SCARF = {
+            "₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "   🧣        ○",
+            "         ╭ ◜◝ ͡ ◜◝ ╮",
+            "         (   zZZ  )",
+            "         ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED_BATH = {
+            "🫧₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "          ╭ ◜◝ ͡ ◜◝ ╮",
+            "          (   zZZ  )",
+            "          ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED_BATH_SCARF = {
+            "🫧₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "      🧣        ○",
+            "           ╭ ◜◝ ͡ ◜◝ ╮",
+            "           (   zZZ  )",
+            "           ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] TIRED_BATH_BOW = {
+            "🫧🎀₍ᐢ◞ . ◟ᐢ₎  ˚ ｡",
+            "            ╭ ◜◝ ͡ ◜◝ ╮",
+            "            (   zZZ  )",
+            "            ╰ ◟◞ ͜ ◟◞ ╯ "
+    };
+
+    public static final String[] HEAL_BOW = {
+            "🎀₍ᐢ×.-ᐢ₎  +  💊  =  🎀₍ᐢ⚈.⚈ᐢ₎ྀ♡"
+    };
+
+    public static final String[] HEAL_SCARF = {
+            "₍ᐢ×.-ᐢ₎  +  💊  =  ₍ᐢ⚈.⚈ᐢ₎ྀ♡",
+            "  🧣                 🧣"
+    };
+
+    public static final String[] HEAL_BATH = {
+            "🫧₍ᐢ×.-ᐢ₎  +  💊  =  🫧₍ᐢ⚈.⚈ᐢ₎ྀ♡"
+    };
+
+    public static final String[] HEAL_BATH_SCARF = {
+            "🫧₍ᐢ×.-ᐢ₎  +  💊  =  🫧₍ᐢ⚈.⚈ᐢ₎ྀ♡",
+            "     🧣                  🧣"
+    };
+
+    public static final String[] HEAL_BATH_BOW = {
+            "🫧🎀₍ᐢ×.-ᐢ₎  +  💊  =  🫧🎀₍ᐢ⚈.⚈ᐢ₎ྀ♡"
+    };
+}
