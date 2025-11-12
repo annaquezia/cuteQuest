efeb2a18368202506a79092aa299a9f7180b582c
Anna Quézia <142363490+annaquezia@users.noreply.github.com>
2025-11-12 18:26:16 -0300

    add: pets

---
+++ src/model/Pet.java
@@ -0,0 +1,213 @@
+package model;
+
+import jakarta.persistence.*;
+import model.decorator.PetComponent;
+import model.decorator.PetDecorator;
+import model.state.PetState;
+import model.state.StateHappy;
+
+@Entity
+@Table(name = "pets")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "species", length = 16)
+public abstract class Pet implements PetComponent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 16)
+    private PetType type;
+
+    @Column(name = "kennel_id", nullable = false)
+    private int kennelID;
+
+    private int health;
+    private int happiness;
+    private int playCount;
+    private int feedCount;
+    private boolean adopted;
+
+    @Transient private PetState currentState;
+    @Transient private PetState lastState;
+    @Transient private PetDecorator acessory;
+    @Transient private PetDecorator bath;
+
+    public Pet(String name, PetType type) {
+        this.id = 0;
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.happiness = 100;
+        this.adopted = false;
+        this.kennelID = 0;
+    }
+
+    public Pet() {}
+
+    public void feed(Pet pet) {
+        currentState.feed(pet);
+        if (bath != null)      bath.feed(this);
+        if (acessory != null)  acessory.feed(this);
+    }
+
+    public void play(Pet pet) {
+        currentState.play(pet);
+        if (bath != null)      bath.play(this);
+        if (acessory != null)  acessory.play(this);
+    }
+
+    public void heal(Pet pet) {
+        currentState.heal(pet);
+        if (bath != null)      bath.heal(this);
+        if (acessory != null)  acessory.heal(this);
+    }
+
+    public void sleep(Pet pet) {
+        currentState.sleep(pet);
+        if (bath != null)      bath.sleep(this);
+        if (acessory != null)  acessory.sleep(this);
+    }
+
+    public void wakeUp(Pet pet) {
+        currentState.wakeUp(pet);
+        if (bath != null)      bath.wakeUp(this);
+        if (acessory != null)  acessory.wakeUp(this);
+    }
+
+
+    public void tick(Pet pet, int hours) {
+        currentState.tick(pet, hours);
+        if (bath != null)      bath.tick(this, hours);
+        if (acessory != null)  acessory.tick(this, hours);
+    }
+
+    public void incrementPlayCount(){
+        playCount++;
+    }
+
+    public void resetPlayCount(){
+        playCount = 0;
+    }
+
+    public void incrementFeedCount(){
+        feedCount++;
+    }
+
+    public void resetFeedCount(){
+        feedCount = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public int getKennelID() {
+        return kennelID;
+    }
+
+    public void setKennelID(int kennelID) {
+        this.kennelID = kennelID;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = clamp(health, 0, 100);
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = clamp(happiness, 0, 100);
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(int playCount) {
+        this.playCount = playCount;
+    }
+
+    public int getFeedCount() {
+        return feedCount;
+    }
+
+    public void setFeedCount(int feedCount) {
+        this.feedCount = feedCount;
+    }
+
+    public boolean isAdopted() {
+        return adopted;
+    }
+
+    public void setAdopted(boolean adopted) {
+        this.adopted = adopted;
+    }
+
+    public PetState getCurrentState() {
+        if (currentState == null) {
+            currentState = new StateHappy();
+        }
+        return currentState;
+    }
+
+    public void setCurrentState(PetState newState) {
+        this.lastState = this.currentState;
+        this.currentState = newState;
+
+    }
+
+    public PetState getLastState() {
+        return lastState;
+    }
+
+    public void setLastState(PetState lastState) {
+        this.lastState = lastState;
+    }
+
+    public PetDecorator getAcessory() {
+        return acessory;
+    }
+
+    public void setAcessory(PetDecorator decorator) {
+        this.acessory = decorator;
+    }
+
+    public PetDecorator getBath() {
+        return bath;
+    }
+
+    public void setBath(PetDecorator bath) {
+        this.bath = bath;
+    }
+
+    private static int clamp(int value, int min, int max) {
+        return Math.max(min, Math.min(max, value));
+    }
+}
